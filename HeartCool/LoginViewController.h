8ea3fa5f9a1c962f59feb1cac41fdb468555b862
Windom <Windom@zwd-macbook-pro.local>
2017-03-02 23:02:16 +0800

    init login view

---
+++ HeartCool/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  HeartCool
+//
+//  Created by Windom on 2017/3/2.
+//  Copyright © 2017年 uteamtec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
